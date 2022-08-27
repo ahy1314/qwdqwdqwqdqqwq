71b993c2296331648d0debb4e2565b5694cb9f8b
ahy1314 <112189188+ahy1314@users.noreply.github.com>
2022-08-27 09:33:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -138,6 +138,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
                 "value": "{} {}".format(today, week),
                 "color": get_color()
             },
+         {
                 “value”：province_name，
                 “color”： get_color()
             },
