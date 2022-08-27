c0125384562cf1af113934d10d8c394f8d043f06
ahy1314 <112189188+ahy1314@users.noreply.github.com>
2022-08-27 10:43:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def get_weather(region):
     temp = response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
     # 当前风向
     wind_dir = response["now"]["windDir"]
-    return weather, temp, wind_dir, temperature
+    return weather, temp, wind_dir
     # 当前最高气温
     max_temperature = response["daily"][0]["temperatureMax"] + u"\N{DEGREE SIGN}" + "C"
     # 当前最低气温
